ec73a49286c8161a5e3ee8eaf45309c45ee38dec
aleeeblablabla <aleee.blablabla@yahoo.com>
2017-04-24 20:08:11 +0200

    Create 13a.sh

---
+++ 13a.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#13 a)Write a shell script that will show on the screen every s seconds, a top
+#of the
+#first n users on the linux system, sorted by the number their running
+#processes (n will be read from keyboard and s from command line).
+echo Read n:
+read n
+while [ 0 ];
+do
+ps -ef > tempfile0
+
+sed 1,1d tempfile0 | awk '{print $1}' | sort | uniq -c | sort -rbn > tempfile
+
+grep -m$n . tempfile | awk '{   print $1" "$2 }'
+rm tempfile
+rm tempfile0
+sleep $1
+echo
+
+done
